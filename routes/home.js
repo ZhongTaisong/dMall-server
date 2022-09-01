91c80fe0c26de9098856948b68c66533526d1c92
zhongtaisong <zhongtaisong@sina.com>
2022-09-01 23:43:53 +0800

    【update - home】try catch

--- routes/home.js
+++ routes/home.js
@@ -6,88 +6,79 @@ const ROUTER_Flag = "HOME";
 /**
  * 查询 - 热门推荐商品
  */
-router.get('/public/hot-recommendations', (req, res) => {
-	req?.pool?.query?.(
-        "SELECT id, mainPicture, price, productName, description FROM dm_products WHERE hot=?", 
-        [101], 
-        (err, result) => {
-            if(err) {
-                return res.status(500).send({
-                    code: `DM-${ ROUTER_Flag }-000001`,
-                    msg: '操作失败!',
-                    error: err,
-                });
-            };
-
-            res.status(200).send({
-                code: "DM-000000",
-                content: result,
-            });
-        }
-    );
+router.get('/public/hot-recommendations', async (req, res) => {
+    try {
+        const content = await new Promise((resolve, reject) => {
+            req?.pool?.query?.(
+                "SELECT id, mainPicture, price, productName, description FROM dm_products WHERE hot=?", 
+                [101], 
+                (err, reuslt) => !err ? resolve(reuslt) : reject(err),
+            );
+        });
+        
+        res.status(200).send({
+            code: "DM-000000",
+            content,
+        });
+    } catch (error) {
+        res.status(500).send({
+            code: `DM-${ ROUTER_Flag }-000001`,
+            msg: '操作失败!',
+            error,
+        });
+    }
 });
 
 /**
  * 查询 - 单品推广商品
  */
-router.get('/public/single-product-promotion', (req, res) => {
-	req?.pool?.query?.(
-        "SELECT id, mainPicture, price, productName, description FROM dm_products WHERE single=?", 
-        [102], 
-        (err, result) => {
-            if(err) {
-                return res.status(500).send({
-                    code: `DM-${ ROUTER_Flag }-000002`,
-                    msg: '操作失败!',
-                    error: err,
-                });
-            };
-
-            res.status(200).send({
-                code: "DM-000000",
-                content: result,
-            });
-        }
-    );
+router.get('/public/single-product-promotion', async (req, res) => {
+    try {
+        const content = await new Promise((resolve, reject) => {
+            req?.pool?.query?.(
+                "SELECT id, mainPicture, price, productName, description FROM dm_products WHERE single=?", 
+                [102], 
+                (err, reuslt) => !err ? resolve(reuslt) : reject(err),
+            );
+        });
+        
+        res.status(200).send({
+            code: "DM-000000",
+            content,
+        });
+    } catch (error) {
+        res.status(500).send({
+            code: `DM-${ ROUTER_Flag }-000002`,
+            msg: '操作失败!',
+            error,
+        });
+    }
 });
 
 /**
  * 查询 - 大图推广商品
  */
-router.get('/public/large-scale-promotion', (req, res) => {
-	req?.pool?.query?.(
-        "SELECT id, bannerPic, description FROM dm_products WHERE banner=?", 
-        [103], 
-        (err, result) => {
-            if(err) {
-                return res.status(500).send({
-                    code: `DM-${ ROUTER_Flag }-000003`,
-                    msg: '操作失败!',
-                    error: err,
-                });
-            };
+router.get('/public/large-scale-promotion', async (req, res) => {
+    try {
+        const content = await new Promise((resolve, reject) => {
+            req?.pool?.query?.(
+                "SELECT id, bannerPic, description FROM dm_products WHERE banner=?", 
+                [103], 
+                (err, reuslt) => !err ? resolve(reuslt) : reject(err),
+            );
+        });
 
-            res.status(200).send({
-                code: "DM-000000",
-                content: result,
-            });
-        }
-    );
-});
-
-// 关键字搜索
-router.get('/kw', (req,res) => {
-    const { kws } = req.query || {};
-    let sql = "SELECT * FROM dm_products WHERE description LIKE ?";
-    let ks = `%${kws}%`;
-    req?.pool?.query?.(sql, [ks], (err, data) => {
-        if(err) throw err;
-        res.send({
+        res.status(200).send({
             code: "DM-000000",
-            data,
-            
-        })
-    });
+            content,
+        });
+    } catch (error) {
+        res.status(500).send({
+            code: `DM-${ ROUTER_Flag }-000003`,
+            msg: '操作失败!',
+            error,
+        });
+    }
 });
 
 module.exports = router;
