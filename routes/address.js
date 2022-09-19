789fc9ef49e8332d9da2d4bf7fec09eee8b770c8
zhongtaisong <zhongtaisong@sina.com>
2022-09-20 00:36:40 +0800

    【update】收货地址

--- routes/address.js
+++ routes/address.js
@@ -1,136 +1,276 @@
 const express = require('express');
 const router = express.Router();
+const kit = require('./../kit');
+// 路由器标识
+const ROUTER_Flag = "ADDRESS";
 
-// 添加收货地址 / 修改收货地址
-router.post('/edit', (req, res) => {
-    const { uname } = req.headers || {};
-    let { id, name, region, detail, phone, isDefault } = req.body || {};
-    if( !uname ){
-        res.status(400).send({
-            code: 1,
-            msg: 'uname不能为空！'
-        })
-        return;
-    }
-    if( !name ){
-        res.status(400).send({
-            code: 2,
-            msg: 'name不能为空！'
-        })
-        return;
-    }
-    if( !region ){
-        res.status(400).send({
-            code: 3,
-            msg: 'region不能为空！'
-        })
-        return;
-    }
-    if( !detail ){
-        res.status(400).send({
-            code: 4,
-            msg: 'detail不能为空！'
-        })
-        return;
-    }
-    if( !phone ){
-        res.status(400).send({
-            code: 5,
-            msg: 'phone不能为空！'
-        })
-        return;
-    }
+/**
+ * 查询 - 收货地址
+ */
+router.get('/select', async (req, res) => {
+    try {
+        const { uname } = req.headers || {};
+        if(!uname) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000002`,
+                msg: '请求头uname不能为空!',
+            });
+        }
 
-    (async () => {
-        let sql=null, params=null, msg=null;
-        if( isDefault ){
-            await new Promise((resolve, reject) => {
-                sql = "UPDATE dm_address SET isDefault=? WHERE uname=?";
-                req?.pool?.query?.(sql, [0, uname], (err, data) => {
-                    if(err) throw err;
-                    resolve();
-                });
+        const content = await new Promise((resolve, reject) => {
+            req?.pool?.query?.(
+                `SELECT * FROM dm_address WHERE uname=? ORDER BY id DESC`, 
+                [uname], 
+                (err, data) => !err ? resolve(data) : reject(err),
+            )
+        });
+
+        if(Array.isArray(content)) {
+            content.forEach(item => {
+                item['isDefault'] = Number(item['isDefault'] || '0');
             })
         }
-
-        await new Promise((resolve, reject) => {
-            if( !id ){
-                sql = "INSERT INTO dm_address VALUES(NULL, ?, ?, ?, ?, ?, ?)";
-                params = [uname, name, region, detail, phone, isDefault ? 1 : 0];
-                msg = '添加';
-            }else{
-                sql = "UPDATE dm_address SET name=?, region=?, detail=?, phone=?, isDefault=? WHERE uname=? AND id=?";
-                params = [name, region, detail, phone, isDefault ? 1 : 0, uname, id];
-                msg = '修改';
-            }
-            req?.pool?.query?.(sql, params, (err, data) => {
-                if(err) throw err;
-                if( data.affectedRows ){
-                    res.send({
-                        code: "DM-000000",
-                        data: null,
-                        msg: `收货地址${msg}成功`
-                    })
-                }else{
-                    res.send({
-                        code: 6,
-                        msg: `收货地址${msg}失败`
-                    })
-                }
-            });
-        })
-    })()
-})
-
-// 查询收货地址
-router.get('/select', (req, res) => {
-    const { uname } = req.headers || {};
-    if( !uname ){
-        res.status(400).send({
-            code: 1,
-            msg: 'uname不能为空！'
-        })
-        return;
-    }
-    
-    let sql = "SELECT * FROM dm_address WHERE uname=? ORDER BY id DESC";
-    req?.pool?.query?.(sql, [uname], (err, data) => {
-        if(err) throw err;
-        res.send({
+        
+        res.status(200).send({
             code: "DM-000000",
-            data,
-            
-        })
-    });
-})
-
-// 删除收货地址
-router.get('/delete', (req, res) => {
-    const { id } = req.query || {};
-    if( !id ){
-        res.status(400).send({
-            code: 1,
-            msg: 'id不能为空！'
-        })
-        return;
+            content,
+        });
+    } catch (error) {
+        res.status(500).send({
+            code: `DM-${ ROUTER_Flag }-000001`,
+            msg: '操作失败!',
+            error,
+        });
     }
-    
-    let sql = "DELETE FROM dm_address WHERE id=?";
-    req?.pool?.query?.(sql, [id], (err, data) => {
-        if(err) throw err;
-        if( data.affectedRows ){    
-            res.send({
-                code: "DM-000000",
-                data: null,
-                msg: '删除收货地址成功'
-            });
-        }else{
-            res.send({
-                code: 2,
-                msg: '删除收货地址失败' 
+});
+
+/**
+ * 添加 - 收货地址
+ */
+router.post('/add', async (req, res) => {
+    try {
+        const { uname } = req.headers || {};
+        const { name, region, detail, phone, isDefault=0 } = req?.body || {};
+        if(!uname) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000004`,
+                msg: '请求头uname不能为空!',
             });
         }
-    });
+
+        if(!name) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000005`,
+                msg: 'name不能为空!',
+            });
+        }
+
+        if(!region) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000006`,
+                msg: 'region不能为空!',
+            });
+        }
+
+        if(!detail) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000007`,
+                msg: 'detail不能为空!',
+            });
+        }
+
+        if(!phone) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000008`,
+                msg: 'phone不能为空!',
+            });
+        }
+
+        if(!kit.validatePhone(phone)) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000009`,
+                msg: '请传入合法的phone!',
+            });
+        }
+
+        if(isDefault){
+            await new Promise((resolve, reject) => {
+                req?.pool?.query?.(
+                    "UPDATE dm_address SET isDefault=? WHERE uname=?", 
+                    [0, uname],
+                    (err, data) => !err ? resolve(Boolean(data?.affectedRows)) : reject(err),
+                )
+            });
+        }
+
+        const reuslt = await new Promise((resolve, reject) => {
+            req?.pool?.query?.(
+                "INSERT INTO dm_address VALUES(NULL, ?, ?, ?, ?, ?, ?)", 
+                [uname, name, region, detail, phone, isDefault],
+                (err, data) => !err ? resolve(Boolean(data?.affectedRows)) : reject(err),
+            )
+        });
+        
+        if(!reuslt) {
+            return res.status(404).send({
+                code: `DM-${ ROUTER_Flag }-000010`,
+                msg: "添加失败!",
+            });
+        }
+
+        res.status(200).send({
+            code: "DM-000000",
+            msg: "添加成功!",
+        });
+    } catch (error) {
+        res.status(500).send({
+            code: `DM-${ ROUTER_Flag }-000003`,
+            msg: '操作失败!',
+            error,
+        });
+    }
+});
+
+/**
+ * 修改 - 收货地址
+ */
+router.put('/update', async (req, res) => {
+    try {
+        const { uname } = req.headers || {};
+        const { id, name, region, detail, phone, isDefault=0 } = req?.body || {};
+        if(!id) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000012`,
+                msg: 'id不能为空!',
+            });
+        }
+
+        if(!name) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000013`,
+                msg: 'name不能为空!',
+            });
+        }
+
+        if(!region) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000014`,
+                msg: 'region不能为空!',
+            });
+        }
+
+        if(!detail) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000015`,
+                msg: 'detail不能为空!',
+            });
+        }
+
+        if(!phone) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000016`,
+                msg: 'phone不能为空!',
+            });
+        }
+
+        if(!kit.validatePhone(phone)) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000017`,
+                msg: '请传入合法的phone!',
+            });
+        }
+        if(!uname) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000018`,
+                msg: '请求头uname不能为空!',
+            });
+        }
+
+        if(isDefault){
+            await new Promise((resolve, reject) => {
+                req?.pool?.query?.(
+                    "UPDATE dm_address SET isDefault=? WHERE uname=?", 
+                    [0, uname],
+                    (err, data) => !err ? resolve(Boolean(data?.affectedRows)) : reject(err),
+                )
+            });
+        }
+
+        const reuslt = await new Promise((resolve, reject) => {
+            req?.pool?.query?.(
+                "UPDATE dm_address SET name=?, region=?, detail=?, phone=?, isDefault=? WHERE uname=? AND id=?", 
+                [name, region, detail, phone, isDefault, uname, id],
+                (err, data) => !err ? resolve(Boolean(data?.affectedRows)) : reject(err),
+            )
+        });
+        
+        if(!reuslt) {
+            return res.status(404).send({
+                code: `DM-${ ROUTER_Flag }-000019`,
+                msg: "修改失败!",
+            });
+        }
+
+        res.status(200).send({
+            code: "DM-000000",
+            msg: "修改成功!",
+        });
+    } catch (error) {
+        res.status(500).send({
+            code: `DM-${ ROUTER_Flag }-000011`,
+            msg: '操作失败!',
+            error,
+        });
+    }
+});
+
+/**
+ * 删除 - 收货地址
+ */
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const { uname } = req.headers || {};
+        const { id } = req?.params || {};
+        if(!uname) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000021`,
+                msg: '请求头uname不能为空!',
+            });
+        }
+
+        if(!id) {
+            return res.status(400).send({
+                code: `DM-${ ROUTER_Flag }-000022`,
+                msg: 'id不能为空!',
+            });
+        }
+
+        const reuslt = await new Promise((resolve, reject) => {
+            req?.pool?.query?.(
+                "DELETE FROM dm_address WHERE id=? AND uname=?", 
+                [id, uname],
+                (err, data) => !err ? resolve(Boolean(data?.affectedRows)) : reject(err),
+            )
+        });
+        
+        if(!reuslt) {
+            return res.status(404).send({
+                code: `DM-${ ROUTER_Flag }-000023`,
+                msg: "删除失败!",
+            });
+        }
+
+        res.status(200).send({
+            code: "DM-000000",
+            msg: "删除成功!",
+        });
+    } catch (error) {
+        res.status(500).send({
+            code: `DM-${ ROUTER_Flag }-000020`,
+            msg: '操作失败!',
+            error,
+        });
+    }
 })
 
 module.exports = router;
