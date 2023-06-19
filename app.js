1714faa3c79a65ca81c2f868be509e84a0f7e0e1
zhongtaisong <zhongtaisong@sina.com>
2023-06-19 21:18:18 +0800

    update

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use('/api/admin/order', require('./routes/admin/order.js'));
 app.use('/api/admin/goods-evaluate', require('./routes/admin/goods-evaluate.js'));
 app.use('/api/admin/goods', require('./routes/admin/goods.js'));
 app.use('/api/admin/user', require('./routes/admin/user.js'));
-app.use('/api/admin', require('./routes/admin.js'));
+app.use('/api/admin/permission', require('./routes/admin/permission.js'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
