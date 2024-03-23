30ae5e5ce64457d3bb291d6a08296ac8639f3945
闹钟太松了 <zhongtaisong@sina.com>
2024-03-23 18:42:45 +0800

    【update】自动运行redis服务

--- redis-client.js
+++ redis-client.js
@@ -1,9 +1,17 @@
 const redis = require("redis");
+
 // 创建redis客户端
 const client = redis.createClient();
 
+// 监听redis成功连接
+client.on('connect', function() {
+    console.log('Redis客户端已连接!');
+});
+
 // 监听redis错误
-client.on('error', (err) => console.log('Redis客户端错误!', err));
+client.on('error', function(err) {
+    console.log('Redis客户端错误!', err);
+});
 
 // 连接redis
 client.connect();
