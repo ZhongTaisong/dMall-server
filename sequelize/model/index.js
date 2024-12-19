76e6bf7f6be989bece31b2fc44994f5e296a3b41
闹钟太松了 <zhongtaisong@sina.com>
2024-12-19 17:55:44 +0800

    update 预设超级管理员账号

--- sequelize/model/index.js
+++ sequelize/model/index.js
@@ -6,6 +6,8 @@ const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.p
     // 设置时区为东八区(国内标准时区)
     timezone: '+08:00',
 });
+const md5 = require('js-md5');
+const kit = require('./../../kit.js');
 
 const db = {};
 const sequelize_config = { Sequelize, sequelize, DataTypes, };
@@ -42,7 +44,34 @@ model_list.forEach(item => {
     }
 })
 
-sequelize.sync().then(() => {
+const UserModel = db['user'];
+sequelize.sync().then(async () => {
+    try {
+        const userCount = await UserModel.count();
+        if(userCount === 0) {
+            const phone = "13100000000";
+            const password = "000000";
+            const pwd = md5(password);
+            await UserModel.create({
+                phone, 
+                password: kit.md5(`${ phone }${ pwd }`), 
+                nickname: "超级管理员",
+                role: "0",
+            });
+            kit.createLogContentFn({
+                path: "sequelize.sync", 
+                msg: "预设用户已创建成功",
+            });
+        }
+        
+    } catch (error) {
+        kit.createLogContentFn({
+            path: "sequelize.sync", 
+            msg: "预设用户已创建失败",
+            error,
+        });
+    }
+
     console.log("数据库同步成功");
 }).catch(err => {
     console.log("数据库同步失败", err);
